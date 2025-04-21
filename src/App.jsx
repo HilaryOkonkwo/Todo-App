1f96538f76ed2ce266eb7e08381a04f16a4744ea
Agu Okonkwo <somtoochukwuhilary@gmail.com>
2025-04-21 09:48:53 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from './Components/Todo'
+import TodoItems from './Components/TodoItems'
+
+
+export const App = () => {
+
+  return (
+    <div>
+      <Todo/>
+  </div>
+  )
+}
